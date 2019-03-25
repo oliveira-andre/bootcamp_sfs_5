d95dab6c3b7f9771195f7e94795ae7b8aca24cf6
Andre Oliveira <desmonplays@gmail.com>
2019-03-25 18:57:25 -0400

    WIP

---
+++ OneBitExchange/spec/system/pages_index_system_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Exchange Currency Process', type: :system, js: true do
+  it 'exchange value' do
+    visit '/'
+    within('#exchange_form') do
+      select('EUR', from: 'source_currency')
+      select('USD', from: 'target_currency')
+      fill_in 'amount', with: '10'
+    end
+    click_button 'CONVERTER'
+
+    expect(page).to have_content('value')
+  end
+end
