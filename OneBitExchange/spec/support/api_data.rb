95b9a309213ad0e2973210b5aa8726b1e6fcd6d9
Andre Oliveira <desmonplays@gmail.com>
2019-03-25 18:04:41 -0400

    (new feature) default response

---
+++ OneBitExchange/spec/support/api_data.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.before(:each) do
+    stub_request(:get, /currencydatafeed.com/)
+      .with(headers: {
+              'Accept' => '*/*'
+            }).to_return(status: 200, body: '
+      {
+        "status": true,
+        "currency": [
+            {
+                "currency": "USD/BRL",
+                "value": "3.41325",
+                "date": "2018-04-20 17:22:59",
+                "type": "original"
+            }
+        ]
+      }', headers: {})
+  end
+end
