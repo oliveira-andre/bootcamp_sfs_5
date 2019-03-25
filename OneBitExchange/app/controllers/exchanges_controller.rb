884953b4e3658770f38c88217bab085fc19552a6
Andre Oliveira <desmonplays@gmail.com>
2019-03-25 18:56:51 -0400

    (new feature) executing the get method of form

---
+++ OneBitExchange/app/controllers/exchanges_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class ExchangesController < ApplicationController
+  def index; end
+
+  def convert
+    value = ExchangeService.new(params[:source_currency],
+                                params[:target_currency],
+                                params[:amount]).perform
+    render json: { "value": value }
+  end
+end
