0948139ae926ede7378df632f320105b0cd4e09d
Andre Oliveira <desmonplays@gmail.com>
2019-03-26 10:05:29 -0400

    (new feature) new fields to model campaigns

--- nosso_amigo_secreto/db/migrate/20190326034749_create_campaigns.rb
+++ nosso_amigo_secreto/db/migrate/20190326034749_create_campaigns.rb
@@ -5,6 +5,9 @@ class CreateCampaigns < ActiveRecord::Migration[5.2]
       t.text :description
       t.references :user, foreign_key: true
       t.integer :status
+      t.datetime :event_date
+      t.string :event_hour
+      t.string :locale
 
       t.timestamps
     end
--- nosso_amigo_secreto/db/schema.rb
+++ nosso_amigo_secreto/db/schema.rb
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2019_03_26_034842) do
     t.text "description"
     t.bigint "user_id"
     t.integer "status"
+    t.datetime "event_date"
+    t.string "event_hour"
+    t.string "locale"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_campaigns_on_user_id"
