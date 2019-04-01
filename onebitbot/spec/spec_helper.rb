4dce7728e6780cc410133d97b599616731115bc9
Andre Oliveira <desmonplays@gmail.com>
2019-04-01 11:19:49 -0400

    (new feature) factories

--- onebitbot/spec/spec_helper.rb
+++ onebitbot/spec/spec_helper.rb
@@ -1,6 +1,11 @@
 require 'rspec'
 require 'rack/test'
+require 'ffaker'
+require "pg_search"
 require_relative '../app.rb'
+
+Dir["./spec/support/**/*.rb"].each { |f| require f }
+Dir["./app/services/**/*.rb"].each {|file| require file }
 
 set :environment, :test
 
@@ -14,4 +19,6 @@ end
 
 RSpec.configure do |c|
   c.include RSpecMixin
+  ActiveRecord::Base.logger = nil
+  ENV['LOG'] == true
 end
